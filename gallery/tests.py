5afcd57beee70cf721d706a1fc806f94ae9c7f5b
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-28 16:55:26 +0300

    admon registration of clasees

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase
+from .models import Category,Location, Image
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.nairobi = Location(location='nairobi')
+        self.nature = Category(category = 'nature')
+        self.monalisa = Image(name = 'monalisa', description = 'beautiful',categorykey = self.nature,
+                              locationkey = self.nairobi)
+
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+    def test_saveimage(self):
+        self.monalisa.saveImage()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>1)
