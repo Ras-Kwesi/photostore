f30e8c465fb8b61b2af01d01d24fc6528c0fa923
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-28 11:52:58 +0300

    Models setup

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Location(models.Model):
+    location = models.CharField(max_length=30)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=20)
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=100)
+    categorykey = models.ForeignKey(Category)
+    locationkey = models.ForeignKey(Location)
