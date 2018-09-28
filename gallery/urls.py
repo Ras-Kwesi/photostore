ab4c5e4d2538575e948c8aa883b07cc96a249063
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-28 17:56:17 +0300

    new routes added

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$',views.index,name='index'),
-
+    url('^image',views.singleimage,name='singleimage')
+    url('^location',views.locateion,name='location')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
