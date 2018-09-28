22456b99bc49950eec666d44dc8e152909d68de4
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-28 15:24:26 +0300

    Picture upload configuration

--- gallery/models.py
+++ gallery/models.py
@@ -19,6 +19,7 @@ class Category(models.Model):
 class Image(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=100)
+    path = models.ImageField(upload_to='picture/',default=True)
     categorykey = models.ForeignKey(Category)
     locationkey = models.ForeignKey(Location)
 
