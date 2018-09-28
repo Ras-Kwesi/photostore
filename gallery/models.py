b39f2d129161f76ded1ed2a7b76bb79639e24e7b
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-28 11:58:09 +0300

    save and add image functions added

--- gallery/models.py
+++ gallery/models.py
@@ -7,13 +7,30 @@ from django.db import models
 class Location(models.Model):
     location = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.location
 
 class Category(models.Model):
     category = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.category
 
 class Image(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=100)
     categorykey = models.ForeignKey(Category)
     locationkey = models.ForeignKey(Location)
+
+    def __str__(self):
+        return self.name
+
+    def saveImage(self):
+        self.save()
+
+
+    def deleteImage(self):
+        self.delete()
+
+    @classmethod
+    def getImage(cls):
